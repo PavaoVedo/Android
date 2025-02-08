565b58669a12fc1a63acba08c4db911ba6757ba7
PavaoVedo <pavster32@gmail.com>
2025-02-08 22:34:14 +0100

    fixed api list problem

--- app/src/main/java/hr/algebra/nasa/api/AnimalApi.kt
+++ app/src/main/java/hr/algebra/nasa/api/AnimalApi.kt
@@ -2,11 +2,12 @@ package hr.algebra.nasa.api
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 const val API_URL = "https://extinct-api.herokuapp.com/"
 
 interface AnimalApi {
-    @GET("api/v1/animal/")
-    fun fetchItems(@Query("count") count: Int = 10) : Call<AnimalResponseDto>
+    @GET("api/v1/animal/{count}")
+    fun fetchItems(@Path("count") count: Int = 10) : Call<AnimalResponseDto>
 }
